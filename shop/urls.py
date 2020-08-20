68001b07cc2bac4564ad8981c9d230976962744f
Tim <han.b.timothy@gmail.com>
2020-08-20 16:46:12 -0700

    Added view and template for detailed item as well as CSS for the site.

--- shop/urls.py
+++ shop/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.item_list, name='item_list'),
+    path('item/<int:pk>/', views.item_detail, name='item_detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Item
 # Create your views here.
 
 def item_list(request):
     items = Item.objects.all().order_by('title')
     return render(request, 'shop/item_list.html', {'items': items})
+
+def item_detail(request,pk):
+    item = get_object_or_404(Item,pk=pk)
+    return render(request, 'shop/item_detail.html', {'item':item})
