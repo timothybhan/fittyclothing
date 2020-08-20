0251a071367867f3fc3b59e685878b20232c41d3
Tim <han.b.timothy@gmail.com>
2020-08-20 15:48:07 -0700

    Modigied templates to display posts from database

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Item
 # Create your views here.
+
+def item_list(request):
+    items = Item.objects.all().order_by('title')
+    return render(request, 'shop/item_list.html', {'items': items})
