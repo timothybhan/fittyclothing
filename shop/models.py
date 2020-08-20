b8a3b48cd1f7f3403a9b17462b1802436eb71bdc
Tim <han.b.timothy@gmail.com>
2020-08-20 15:21:28 -0700

    My Fitting Clothing app, first commit

---
+++ shop/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+CATEGORY_CHOICES = (
+    ('S', 'Shirt'),
+    ('SW', 'Sport wear'),
+    ('OW', 'Outwear')
+)
+
+LABEL_CHOICES = (
+    ('P', 'primary'),
+    ('S', 'secondary'),
+    ('D', 'danger')
+)
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    discount_price = models.FloatField(blank=True, null=True)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    label = models.CharField(choices=LABEL_CHOICES, max_length=1)
+    slug = models.SlugField()
+    description = models.TextField()
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.title
