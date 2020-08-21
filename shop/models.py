817a25dd390375eb667b1d7b376c381386063900
Tim <han.b.timothy@gmail.com>
2020-08-20 17:02:30 -0700

    added photos to static

--- shop/models.py
+++ shop/models.py
@@ -21,7 +21,7 @@ class Item(models.Model):
     label = models.CharField(choices=LABEL_CHOICES, max_length=1)
     slug = models.SlugField()
     description = models.TextField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to='static/productimages')
 
     def __str__(self):
         return self.title
